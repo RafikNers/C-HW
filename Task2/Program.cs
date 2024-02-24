0eabfa1f52cdff731867a625ec8b6c2c01df32ab
Rafik Nersisyan <Diisya@yandex.ru>
2024-02-24 06:05:06 +0300

    Add S7HW:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿int AckermannFunc(int m, int n)
+    {
+        if (m < 0 || n < 0) throw new ArgumentOutOfRangeException();
+        if (m == 0) return n + 1;
+        if (n == 0) return AckermannFunc(m - 1, 1);
+        return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+    }
+
+Console.WriteLine(AckermannFunc(3, 5));
+Console.WriteLine(AckermannFunc(1, 2));
