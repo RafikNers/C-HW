81132599e6d01ec33dfa98112bed6b6121a23454
Rafik Nersisyan <Diisya@yandex.ru>
2024-02-24 05:07:33 +0300

    Add S7HW:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void RangeOfDigit( int m, int n)
+{   
+    if (m == n)
+    {
+        System.Console.WriteLine(m);
+        return;
+    }
+    System.Console.Write(m + ",");
+    RangeOfDigit(m+1, n);
+}
+
+
+RangeOfDigit(1,5);
