92821991984b274ca51771aa2d88c1079c8fa32c
Rafik Nersisyan <Diisya@yandex.ru>
2024-02-24 07:53:19 +0300

    Add S7HW:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] numbers = [2,5,43,56,4,3,2,1];
+int size = numbers.Length -1;
+
+void MassiveDigitsPrint(int [] numbers, int  size )
+{
+    if(size < 1)
+    {
+        System.Console.Write(numbers[size]);
+        System.Console.WriteLine();
+        return;
+    }
+    if (size < numbers.Length)
+    {
+        System.Console.Write(numbers[size]+ ",");
+        MassiveDigitsPrint(numbers,size-1);
+    }
+    
+}
+
+MassiveDigitsPrint(numbers, size);
+
+
